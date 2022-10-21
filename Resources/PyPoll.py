adf3c2ef7216f663df3f8a3f51b3cb47feaf8bf0
amramy <amr_amy@hotmail.com>
2022-10-20 21:38:12 -0400

    Updateing Resources/PyPoll.py with canidate info file.

--- Resources/PyPoll.py
+++ Resources/PyPoll.py
@@ -29,9 +29,9 @@ import os
 file_to_load = os.path.join("Election_Analysis","Resources", "election_results.csv")
 
 #open the election results as read file
-with open(file_to_load) as election_data:
+##with open(file_to_load) as election_data:
     #print the file object
-    print(election_data)
+    ##print(election_data)
         #OuTPUT <_io.TextIOWrapper name='Election_Analysis\\Resources\\election_results.csv' mode='r' encoding='cp1252'>
 
 #To Write a file to a diectory
@@ -69,6 +69,25 @@ with open(file_to_save, "w") as txt_file:
 
 
 
+#INSERTED TOTAL VOTES COUNT
+#1. Initialize a total vote counter.
+total_votes = 0
+
+
+#INSERTED 
+#Declare new List: Candidate names
+candidate_options = []
+
+#INSERTED
+#Create Dictionary for Candidate and number of votes
+#Dictionarys have the curlyS {}
+candidate_votes = {}
+
+#INSERTED
+#Find Vote %
+#vote_percent = (votes / total_votes) * 100
+
+#Open the election results and read the file
 with open(file_to_load) as election_data:
 
 
@@ -80,18 +99,79 @@ with open(file_to_load) as election_data:
 #PRint just the Header ROW
 #Remember to INDEX!!!
     headers = next(file_reader)
-    print(headers)
+    ##print(headers)
 
 
 #print each row in the csv file.
-#   for row in file_reader:
-#       print(row)
+    for row in file_reader:
+    #2. INSERTED add to the total vote count.
+        total_votes += 1
 
+        #commented print row out so it will print votes... see changes
+       ##print(row)
+        #3. Print the toal Votes REMEMBER to index!
+        ##print(total_votes)
+
+        #INSERT
+        #finding candidate names (running through data starting on row 2 under Header)
+        candidate_name = row[2]
+        #Print candidate name from each row...... This printed all the lines candidate name....
+        ##print(candidate_name)
+        
+
+        #InSERT 
+        #if the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            #Add it to the listof candidates
+
+        #INSERT 
+        #add candidate name to the candidate list.......THis printed all the candidates... not in a list. like a checkerboard.
+                                                        ###UNTIL added the if statement above. then it listed the 3 candidates :)
+            candidate_options.append(candidate_name)
+
+            ##Begin tracking candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+            #NEED to increase (increment) votes by 1 every time candidates name appears in a row
+            ###NOTE needed to align indent with IF 
+        candidate_votes[candidate_name] += 1
+
+    ##Determint % of votes for each candidate by looping through the counts.
+    #1. Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+        #2Retrieve vote count of a candidte.
+        votes = candidate_votes[candidate_name]
+        #3calculate percentage of votes  
+        # NOTE I removed the * 100 that was at the end of the string and put it in the  print f string
+        votes_percentage = float(votes) / float(total_votes) 
+
+
+        #print candidate list
+            #print(candidate_options)
+
+            #Print candidate vote dictionary......This printed Candidate and Votes AT ZERO!, 
+        #print(candidate_votes)
+                #{'Charles Casper Stockham': 85213, 'Diana DeGette': 272892, 'Raymon Anthony Doane': 11606}
+
+
+         #4 Print the candidate name and percent votes
+         # Removed * 100 from votes_percent above and added the *100:.1f to make the % one decimal.
+        print(f'{candidate_name}: recieved {votes_percentage * 100:.1f} % of the vote.')
 
 #Total number of votes cast
+
+
 #A complete list of candidates who received votes
+
+
 #Total number of votes each candidate received
+    #{'Charles Casper Stockham': 85213, 'Diana DeGette': 272892, 'Raymon Anthony Doane': 11606}
+
 #Percentage of votes each candidate won
+    #Charles Casper Stockham: recieved 23.04854332167558 % of the vote.
+    #Diana DeGette: recieved 73.81224794501652 % of the vote.
+    #Raymon Anthony Doane: recieved 3.1392087333079077 % of the vote.
+
 #The winner of the election based on popular vote
 
 
